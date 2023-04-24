e2b4c61e459b959caf2614d3671153303e276f3a
Michael Koulouvaris <mikekoul@gmail.com>
2023-04-24 14:28:38 -0600

    Add method to parse the csv data into an array

--- lib/encodes.rb
+++ lib/encodes.rb
@@ -4,4 +4,17 @@ class Encodes
   def initialize(file_path)
     @file_path = file_path
   end
+
+  def parse_csv
+    encodes_array = []
+
+    CSV.foreach(@file_path, headers: true) do |row|
+      row_data = {}
+      row.headers.each do |header|
+        row_data[header] = row[header]
+      end
+      encodes_array << row_data
+    end
+    encodes_array
+  end
 end
--- spec/encodes_spec.rb
+++ spec/encodes_spec.rb
@@ -10,8 +10,8 @@ RSpec.describe Encodes do
   end
 
   it 'parses the data and returns an array' do
-    expect(@encode.parse_data).to be_a(Array)
-    expect(@encode.parse_data[0]).to eq(
+    expect(@encode.parse_csv).to be_a(Array)
+    expect(@encode.parse_csv[0]).to eq(
       {"long_url"=>"https://google.com/",    "domain"=>"bit.ly", 
       "hash"=>"31Tt55y"
       })
