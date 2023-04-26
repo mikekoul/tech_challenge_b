202acfdf01bac5e264c7e5f34f89701434e64f7c
Michael Koulouvaris <mikekoul@gmail.com>
2023-04-25 18:00:41 -0600

    Add function to return the amount of times a link is clicked per each website

--- lib/click_counter.rb
+++ lib/click_counter.rb
@@ -9,5 +9,19 @@ class ClickCounter
     @decode_results = Decodes.new(json_file).parse_2021_entries 
   end
 
-  
+  def count_clicks(encode_results, decode_results)
+    result_hash = {}
+
+    d_results = @decode_results.map { |d| d['bitlink'][-7..-1] }
+    
+    @encode_results.each do |e_result|
+      long_url = e_result['long_url'] 
+      hash = e_result['hash']
+      count = d_results.count(hash)
+      result_hash[long_url] = count
+    end
+
+    format_array = result_hash.map { |long_url, count| { long_url => count } }
+    format_array.sort_by { |entry| -entry.values.first }  
+    end
 end
--- spec/click_counter_spec.rb
+++ spec/click_counter_spec.rb
@@ -11,6 +11,13 @@ RSpec.describe ClickCounter do
     expect(@click_counter).to be_an_instance_of(ClickCounter)
   end
 
-  
-
+  it 'can count the matching entries for each bitlink and format the data in descending order' do
+    expect(@click_counter.count_clicks(@encode_results, @decode_results)).to eq(
+      [{"https://youtube.com/"=>557},
+      {"https://twitter.com/"=>512},
+      {"https://reddit.com/"=>510},
+      {"https://github.com/"=>497},
+      {"https://linkedin.com/"=>496},
+      {"https://google.com/"=>492}])
+  end
 end
